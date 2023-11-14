1739963b728f1a11625d36e574f56f857d2178e6
arthurbright <artb1234567@gmail.com>
2023-11-14 00:50:16 -0500

    add interactivity!

--- sim.py
+++ sim.py
@@ -1,4 +1,8 @@
 from grid8 import grid, SIZE, INPUT
+RED = '\033[91m'
+YELLOW = '\033[92m'
+ENDC = '\033[0m'
+
 
 r = 0
 c = 0
@@ -9,9 +13,14 @@ def isValidMove(r1, c1, r2, c2):
         return True
     return False
 
+def translate(move):
+    newr = int(move[1]) - 1
+    newc = ord(move[0]) - ord('a')
+    return (newr, newc)
+
 ind = 0
 totalTime = 0
-for time, move in INPUT:
+while True:
     # find all and wait
     cur = grid[r][c]
     sames = []
@@ -21,6 +30,17 @@ for time, move in INPUT:
                 sames.append((i, j))
     
     n = len(sames)
+
+    print(f"n = {n}")
+
+    if ind < len(INPUT):
+        time, move = INPUT[ind]
+        READINPUT =  True
+    else:
+        time, move = input("Next move: ").split()
+        time = float(time)
+        READINPUT = False
+
     diff = float(time)/n
 
     if grid[SIZE - 1 - r][SIZE - 1 - c] != cur:
@@ -30,26 +50,37 @@ for time, move in INPUT:
         grid[i][j] -= diff
 
     # move
-    newr = int(move[1]) - 1
-    newc = ord(move[0]) - ord('a')
+    newr, newc = translate(move)
 
     if isValidMove(r, c, newr, newc):
         r = newr
         c = newc
+        totalTime += time
+        ind += 1
     else:
         print(f"ILLEGAL MOVE: ({time}, {move})  (move {ind})")
-        break
-    ind += 1
-    totalTime += time
+        if grid[SIZE - 1 - r][SIZE - 1 - c] != cur:
+            grid[SIZE - 1 - r][SIZE - 1 - c] -= diff
 
-# print result
-print(f"Final coords: ({r}, {c})")
-print(f"Total time: {totalTime}")
-print("=================================================")
+        for i, j in sames:
+            grid[i][j] += diff
+        if READINPUT:
+            break
 
-SPACING = 7
-for i in range(SIZE):
-    string = ""
-    for j in range(SIZE):
-        string += (f"{'%g'%(round(grid[i][j], SPACING - 5))}").ljust(SPACING)
-    print(string)
+    # print result
+    print("")
+    print(f"Total time: {totalTime}")
+    print("      a      b      c      d      e      f      g      h    ")
+    print("   ======================================================")
+
+    SPACING = 7
+    for i in range(SIZE):
+        string = f"{i + 1} |   "
+        for j in range(SIZE):
+            if (i, j) == (r, c):
+                string += RED + (f"{'%g'%(round(grid[i][j], SPACING - 5))}").ljust(SPACING) + ENDC
+            elif grid[r][c] == grid[i][j]:
+                string += YELLOW + (f"{'%g'%(round(grid[i][j], SPACING - 5))}").ljust(SPACING) + ENDC
+            else:
+                string += (f"{'%g'%(round(grid[i][j], SPACING - 5))}").ljust(SPACING) 
+        print(string)
